f470e57fa0407b33e29b22714866248a36e0b540
NEZZ <omarkamoun97@gmail.com>
2019-04-07 01:42:55 +0100

    home

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -5,7 +5,6 @@ import { NgxEchartsModule } from 'ngx-echarts';
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
 import { StatusCardComponent } from './status-card/status-card.component';
-import { ContactsComponent } from './contacts/contacts.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { RoomSelectorComponent } from './rooms/room-selector/room-selector.component';
 import { TemperatureComponent } from './temperature/temperature.component';
@@ -30,7 +29,6 @@ import { TrafficChartComponent } from './traffic/traffic-chart.component';
     DashboardComponent,
     StatusCardComponent,
     TemperatureDraggerComponent,
-    ContactsComponent,
     RoomSelectorComponent,
     TemperatureComponent,
     RoomsComponent,
